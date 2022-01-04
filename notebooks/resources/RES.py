1bef1902b593e19bbe9b7acb3877c5ba8beb54b7
Maxketelaar <35805649+Maxketelaar@users.noreply.github.com>
2022-01-04 20:10:13 +0100

    objective functions

--- notebooks/resources/RES.py
+++ notebooks/resources/RES.py
@@ -2,10 +2,16 @@ import numpy as np
 import scipy as sp
 import topogenesis as tg
 import pyvista as pv
+import trimesh as tm
+
+def transform_mat(value):
+    mat = np.identity(4)
+    mat[:3,-1] = np.array(value)
+    return mat
 
 # ------------------------------------------------------ #
 # convert trimesh object to pyvista object
-# from: SAzadadi
+# from: AnastasiaFlorou
 # ------------------------------------------------------ #
 def tri_to_pv(tri_mesh):
     faces = np.pad(tri_mesh.faces, ((0, 0),(1,0)), 'constant', constant_values=3)
@@ -232,3 +238,361 @@ def ground_graph(reference_vectors, hitface_id, ray_id, envelope_lattice, faces_
             U[vox_id, vector] = 1
     
     return G, U 
+
+# ------------------------------------------------------ #
+# find centroids of voxels with no neighbours in 
+# certain directions
+# From: adapted with help from Shervin for origin
+# ------------------------------------------------------ #
+
+def find_centroids(lattice, ref_lattice, dir, axes):
+
+    # shifting all the voxels one level in a certain direction
+    shifted_lattice_Y_pos = np.roll(lattice, (0,0,dir),axis=axes) 
+    
+    # an exposed facade surface exists where a voxel is filled (1) and the voxel next to it is empty (0)
+    side_voxels_3darray_padded = (lattice == 1) *  (shifted_lattice_Y_pos == 0)
+
+    # removing the pad
+    side_voxels_3darray = side_voxels_3darray_padded[1:-1, 1:-1, 1:-1]
+    
+    # convert to lattice
+    side_voxels_lattice = tg.to_lattice(side_voxels_3darray, ref_lattice)
+
+    # extracting the centroids of all exposed voxels
+    centroids = side_voxels_lattice.centroids
+
+    return centroids
+
+# ------------------------------------------------------ #
+# constructing meshes for all 5 directions of a voxel 
+# floor is not used
+# From: adapted with help from Shervin for origin
+# ------------------------------------------------------ #
+ 
+def construct_mesh_y_pos(centroid, unit):
+    meshes= []
+    for i, cen in enumerate(centroid):
+        # generating the vertices of the side faces in +Y direction
+        # centroid + half of the unit size in the four top directions
+        v0 = cen + 0.5 * unit * np.array([ 1, -1, 1]) # side right above
+        v1 = cen + 0.5 * unit * np.array([ 1,-1, -1]) # side right below
+        v2 = cen + 0.5 * unit * np.array([-1,-1, -1]) # side left below
+        v3 = cen + 0.5 * unit * np.array([-1, -1, 1]) # side left above
+
+        face_a = [v0,v1,v2] # trimesh only takes triangular meshes, no quad meshes
+        face_b = [v2,v3,v0]
+
+        mesh_a = tm.Trimesh(vertices= face_a, faces= [[0,2,1]])
+        mesh_b = tm.Trimesh(vertices= face_b, faces= [[0,2,1]])
+
+        meshes.append(mesh_a)
+        meshes.append(mesh_b)
+
+    return meshes
+
+def construct_mesh_y_neg(centroid, unit):
+    meshes= []
+    for i, cen in enumerate(centroid):
+        # generating the vertices of the side faces in +Y direction
+        # centroid + half of the unit size in the four top directions
+        v0 = cen + 0.5 * unit * np.array([ 1, 1, 1]) # side right above
+        v1 = cen + 0.5 * unit * np.array([ 1,1, -1]) # side right below
+        v2 = cen + 0.5 * unit * np.array([-1,1, -1]) # side left below
+        v3 = cen + 0.5 * unit * np.array([-1, 1, 1]) # side left above
+
+        face_a = [v1,v2,v3] # trimesh only takes triangular meshes, no quad meshes
+        face_b = [v3,v0,v1]
+
+        mesh_a = tm.Trimesh(vertices= face_a, faces= [[1,2,0]])
+        mesh_b = tm.Trimesh(vertices= face_b, faces= [[1,2,0]])
+
+        meshes.append(mesh_a)
+        meshes.append(mesh_b)
+
+    return meshes
+
+def construct_mesh_x_pos(centroid, unit):
+    meshes= []
+    for i, cen in enumerate(centroid):
+        # generating the vertices of the side faces in +Y direction
+        # centroid + half of the unit size in the four top directions
+        v0 = cen + 0.5 * unit * np.array([ -1, -1, 1]) # side right above
+        v1 = cen + 0.5 * unit * np.array([ -1,-1, -1]) # side right below
+        v2 = cen + 0.5 * unit * np.array([-1,1, -1]) # side left below
+        v3 = cen + 0.5 * unit * np.array([-1, 1, 1]) # side left above
+
+        face_a = [v0,v1,v2] # trimesh only takes triangular meshes, no quad meshes
+        face_b = [v2,v3,v0]
+
+        mesh_a = tm.Trimesh(vertices= face_a, faces= [[2,1,0]])
+        mesh_b = tm.Trimesh(vertices= face_b, faces= [[2,1,0]])
+
+        meshes.append(mesh_a)
+        meshes.append(mesh_b)
+
+    return meshes
+
+def construct_mesh_x_neg(centroid, unit):
+    meshes= []
+    for i, cen in enumerate(centroid):
+        # generating the vertices of the side faces in +Y direction
+        # centroid + half of the unit size in the four top directions
+        v0 = cen + 0.5 * unit * np.array([ 1, 1, 1]) # side right above
+        v1 = cen + 0.5 * unit * np.array([1,1, -1]) # side right below
+        v2 = cen + 0.5 * unit * np.array([1,-1, -1]) # side left below
+        v3 = cen + 0.5 * unit * np.array([1, -1, 1]) # side left above
+
+        face_a = [v0,v1,v2] # trimesh only takes triangular meshes, no quad meshes
+        face_b = [v2,v3,v0]
+
+        mesh_a = tm.Trimesh(vertices= face_a, faces= [[2,1,0]])
+        mesh_b = tm.Trimesh(vertices= face_b, faces= [[2,1,0]])
+
+        meshes.append(mesh_a)
+        meshes.append(mesh_b)
+
+    return meshes
+
+def construct_mesh_z_pos(centroid, unit):
+    meshes= []
+    for i, cen in enumerate(centroid):
+        # generating the vertices of the side faces in +Y direction
+        # centroid + half of the unit size in the four top directions
+        v0 = cen + 0.5 * unit * np.array([ 1, 1, 1]) # side right above
+        v1 = cen + 0.5 * unit * np.array([ 1,-1, 1]) # side right below
+        v2 = cen + 0.5 * unit * np.array([-1,-1, 1]) # side left below
+        v3 = cen + 0.5 * unit * np.array([-1, 1, 1]) # side left above
+
+        face_a = [v0,v1,v2] # trimesh only takes triangular meshes, no quad meshes
+        face_b = [v2,v3,v0]
+
+        mesh_a = tm.Trimesh(vertices= face_a, faces= [[0,1,2]])
+        mesh_b = tm.Trimesh(vertices= face_b, faces= [[0,1,2]])
+
+        meshes.append(mesh_a)
+        meshes.append(mesh_b)
+
+    return meshes
+
+# ------------------------------------------------------ #
+# constructing meshes and normals/points for each face 
+# 
+# From: adapted with help from Shervin for origin
+# ------------------------------------------------------ #
+
+# this function could be much shorter probably --> 4x almost identical operations, does works fine though
+
+def construct_vertical_mesh(lat,unit):
+    vertical_meshes = []
+    test_points = []
+    test_point_normals = []
+
+    # padding to avoid the rolling issue
+    input_lattice_padded = np.pad(lat, 1, mode='constant',constant_values=0)
+
+    # Y_positive mesh
+    Y_pos_centroids = find_centroids(lattice= input_lattice_padded, ref_lattice=lat, dir= 1, axes= 1)
+    Y_pos_mesh = construct_mesh_y_pos(Y_pos_centroids, unit)
+    vertical_meshes.extend(Y_pos_mesh)
+
+    # move the test points so they are not inside mesh edge
+    Y_pos_text_point = Y_pos_centroids + (lat.unit/2 + 0.01) * [0,-1,0]
+    test_points.extend(Y_pos_text_point)
+
+    # find normal of squares: take every other triangle normal
+    Y_pos_mesh = tm.util.concatenate(Y_pos_mesh)
+    Y_pos_normals = Y_pos_mesh.face_normals[::2]
+    test_point_normals.extend(Y_pos_normals)
+    
+    # Y_negative mesh
+    Y_neg_centroids = find_centroids(lattice= input_lattice_padded, ref_lattice=lat, dir= -1, axes= 1)
+    Y_neg_mesh = construct_mesh_y_neg(Y_neg_centroids, unit)
+    vertical_meshes.extend(Y_neg_mesh)
+
+    # move the test points so they are not inside mesh edge
+    Y_neg_text_point = Y_neg_centroids + (lat.unit/2 + 0.01) * [0,1,0]
+    test_points.extend(Y_neg_text_point)
+
+    # find normal of squares: take every other triangle normal
+    Y_neg_mesh = tm.util.concatenate(Y_neg_mesh)
+    Y_neg_normals = Y_neg_mesh.face_normals[::2]
+    test_point_normals.extend(Y_neg_normals)
+
+    # X_positive mesh
+    X_pos_centroids = find_centroids(lattice= input_lattice_padded, ref_lattice=lat, dir= 1, axes= 0)
+    X_pos_mesh = construct_mesh_x_pos(X_pos_centroids, unit)
+    vertical_meshes.extend(X_pos_mesh)
+
+    # move the test points so they are not inside mesh edge
+    X_pos_text_point = X_pos_centroids + (lat.unit/2 + 0.01) * [-1,0,0]
+    test_points.extend(X_pos_text_point)
+
+    # find normal of squares: take every other triangle normal
+    X_pos_mesh = tm.util.concatenate(X_pos_mesh)
+    X_pos_normals = X_pos_mesh.face_normals[::2]
+    test_point_normals.extend(X_pos_normals)
+
+    # X_negative mesh
+    X_neg_centroids = find_centroids(lattice= input_lattice_padded, ref_lattice=lat, dir= -1, axes= 0)
+    X_neg_mesh = construct_mesh_x_neg(X_neg_centroids, unit)
+    vertical_meshes.extend(X_neg_mesh)
+
+    # move the test points so they are not inside mesh edge
+    X_neg_text_point = X_neg_centroids + (lat.unit/2 + 0.01) * [1,0,0]
+    test_points.extend(X_neg_text_point)
+
+    # find normal of squares: take every other triangle normal
+    X_neg_mesh = tm.util.concatenate(X_neg_mesh)
+    X_neg_normals = X_neg_mesh.face_normals[::2]
+    test_point_normals.extend(X_neg_normals)
+
+    return vertical_meshes, test_points, test_point_normals
+
+def construct_horizontal_mesh(lat,unit):
+    horizontal_meshes = []
+    test_points = []
+    test_point_normals = []
+
+    # padding to avoid the rolling issue
+    input_lattice_padded = np.pad(lat, 1, mode='constant',constant_values=0)
+
+    # Z_positive mesh
+    Z_pos_centroids = find_centroids(lattice= input_lattice_padded, ref_lattice=lat, dir= -1, axes= 2)
+    Z_pos_mesh = construct_mesh_z_pos(Z_pos_centroids, unit)
+    horizontal_meshes.extend(Z_pos_mesh)
+
+    # move the test points so they are not inside mesh edge
+    Z_pos_text_point = Z_pos_centroids + (lat.unit/2 + 0.01) * [0,0,1]
+    test_points.extend(Z_pos_text_point)
+
+    # find normal of squares: take every other triangle normal
+    Z_pos_mesh = tm.util.concatenate(Z_pos_mesh)
+    Z_pos_normals = Z_pos_mesh.face_normals[::2]
+    test_point_normals.extend(Z_pos_normals) 
+
+    return horizontal_meshes, test_points, test_point_normals
+
+# ------------------------------------------------------ #
+#  
+# OBJECTIVE FUNCTIONS
+# 
+# ------------------------------------------------------ #
+
+
+def crit_1_PV(variables, ref_lattice, vector, magnitude, environment):
+    # first we create the lattice for the current configuration
+    lattice = reshape_and_store_to_lattice(variables, ref_lattice)
+
+    # create vertical and horizontal test points, meshes, and normals
+    horizontal_meshes, horizontal_test_points, horizontal_test_point_normals = construct_horizontal_mesh(lattice,lattice.unit)
+    vertical_meshes, vertical_test_points, vertical_test_points_normals = construct_vertical_mesh(lattice, lattice.unit)
+
+    # combine the meshes
+    roof_mesh = tm.util.concatenate(horizontal_meshes)
+    facade_mesh = tm.util.concatenate(vertical_meshes)
+    building_mesh = tm.util.concatenate(roof_mesh,facade_mesh)
+    combined_meshes = tm.util.concatenate(building_mesh, environment)
+
+    # shoot towards the skydome points from all of the voxels
+    ray_per_ctr = np.tile(vector, [len(horizontal_test_points),1]) # daylighting ray for each centroid
+    ctr_per_ray = np.tile(horizontal_test_points, [1, len(vector)]).reshape(-1, 3) # daylighting centroid for each ray
+    val_per_ray = np.tile(magnitude, [len(horizontal_test_points),1]) # solar ray intensity on horizontal surface per m2 for each test centroid    
+
+    # USING EMBREE, ray tracing is much faster: any hit means ray is blocked, we don't need all hits. Hits that DO make it: x intensity
+    ray_hit = combined_meshes.ray.intersects_any(ray_origins= ctr_per_ray, ray_directions= -ray_per_ctr)
+    blocked_rays = np.logical_not(ray_hit)
+    
+    # values per ray
+    values = np.multiply(lattice.unit[0]*lattice.unit[1] * val_per_ray.flatten(), blocked_rays) 
+
+    # Wh received on roof over the year
+    crit_1_PV_potential = np.sum(values,dtype=np.int64) # overflow with 32 bits
+
+    # values mapped per mesh pair (values for each surface)
+    crit_1_pervox = np.sum(values.reshape(len(horizontal_test_points),len(vector)),axis=1, dtype=np.int64) 
+
+    return crit_1_PV_potential, crit_1_pervox
+
+
+def crit_2_DL(variables, ref_lattice, vector, magnitude, environment):
+    # first we create the lattice for the current configuration
+    lattice = reshape_and_store_to_lattice(variables, ref_lattice)
+
+    # create vertical and horizontal test points, meshes, and normals
+    horizontal_meshes, horizontal_test_points, horizontal_test_point_normals = construct_horizontal_mesh(lattice,lattice.unit)
+    vertical_meshes, vertical_test_points, vertical_test_points_normals = construct_vertical_mesh(lattice, lattice.unit)
+
+    # combine the meshes
+    roof_mesh = tm.util.concatenate(horizontal_meshes)
+    facade_mesh = tm.util.concatenate(vertical_meshes)
+    building_mesh = tm.util.concatenate(roof_mesh,facade_mesh)
+    combined_meshes = tm.util.concatenate(building_mesh, environment)
+
+    # shoot towards the skydome points from all of the voxels
+    ray_per_ctr = np.tile(vector, [len(vertical_test_points),1]) # daylighting ray for each centroid
+    ctr_per_ray = np.tile(vertical_test_points, [1, len(vector)]).reshape(-1, 3) # daylighting centroid for each ray
+    val_per_ray = np.tile(magnitude, [len(vertical_test_points),1]) # solar ray intensity on horizontal surface per m2 for each test centroid    
+
+    # USING EMBREE, ray tracing is much faster: any hit means ray is blocked, we don't need all hits. Hits that DO make it: x intensity
+    ray_hit = combined_meshes.ray.intersects_any(ray_origins= ctr_per_ray, ray_directions= -ray_per_ctr)
+    blocked_rays = np.logical_not(ray_hit)
+
+    # values per ray in lux*100
+    values = np.multiply(lattice.unit[0]*lattice.unit[1] * val_per_ray.flatten(), blocked_rays) 
+
+    # lux*100 received over the year
+    crit_2_DL_potential = np.sum(values,dtype=np.int64) 
+
+    # values mapped per mesh pair (values for each surface)
+    crit_2_pervox = np.sum(values.reshape(len(vertical_test_points),len(vector)),axis=1, dtype=np.int64) 
+
+    return crit_2_DL_potential, crit_2_pervox
+
+
+def crit_3_rc(variables, ref_lattice):
+    # create the current configuration as a lattice
+    curr_envelope = reshape_and_store_to_lattice(variables.astype('bool'), ref_lattice)
+    
+    # flatten the envelope
+    envlp_voxs = curr_envelope.flatten()
+
+    # create stencil
+    stencil = tg.create_stencil("von_neumann", 1, 1)
+    stencil.set_index([0,0,0], 0)
+
+    # find indices of the neighbours for each voxel 
+    neighs = curr_envelope.find_neighbours(stencil)
+
+    # occupation status for the neighbours for each voxel
+    neighs_status = envlp_voxs[neighs]
+
+    # for voxels inside the envelope:
+    neigh_array = np.array(neighs_status[envlp_voxs.astype("bool")])  
+
+    # when the neighbour's status is False that refers to an outer face
+    outer_faces = np.count_nonzero(neigh_array==0)
+
+    # voxel edge length
+    l = ref_lattice.unit[0] # TODO: can we leave this dimension out?
+
+    # calculate total surface area of outer faces
+    A_exterior = (l**2)*outer_faces
+
+    # number of in-envelope voxels
+    in_voxels = np.count_nonzero(variables)
+
+    # calculate total volume inclosed in the envelope
+    V = in_voxels * (l**3)
+
+    # edge length of a cube that has the same volume
+    l_ref = V**(1/3)
+
+    # calculate ratio
+    R_ref = (6*(l_ref**2))/V
+
+    # dimensionless indicator heat retention potential
+    crit_3_RC = (A_exterior/V)/R_ref
+    
+    return crit_3_RC
+
